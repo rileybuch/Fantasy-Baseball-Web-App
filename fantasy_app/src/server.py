e1bf4dfeadb760e78e949d13a00897e4ee6649c9
Chris <ktranman@gmail.com>
2020-03-17 21:23:34 -0700

    simple app and scripts to seed some data

--- fantasy_app/src/server.py
+++ fantasy_app/src/server.py
@@ -1,19 +1,32 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort,send_from_directory,send_file,jsonify
+from flask_mysqldb import MySQL
 import pandas as pd
 
 import json
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'dbo'
+
+mysql = MySQL(app)
+
 @app.route("/")
-def hello(): 
-    print('main........')  
-    df = pd.read_csv('data/batting_data_1996_2019.csv')
-    data = df['Team'].unique() 
-    #json_data = df.to_json(orient='records')[1:-1].replace('},{', '} {')
-    json_data = json.dumps(data.tolist())
-    print(json_data)
-    return render_template('index.html', data=json_data)
+def index(): 
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT DISTINCT Name FROM dbo.BattingDemo")
+    data = cur.fetchall()
+    data_return = [entry[0] for entry in data]
+    return render_template('index.html', data=data_return)
+
+@app.route("/<player_name>")
+def get_player_stats(player_name):
+	cur = mysql.connection.cursor()
+	cur.execute("SELECT * FROM dbo.BattingDemo WHERE Name = %s ORDER BY Season", (player_name,))
+	player_data = cur.fetchall()
+	return render_template('data.html', data=player_data)
 
 # run the application
 if __name__ == "__main__":
